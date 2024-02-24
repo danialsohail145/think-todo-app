e648ee8c189d8c2515fe9f933e3dde612130c872
Danial Sohail <danialsohail145@gmail.com>
2024-02-24 18:27:16 +0500

    Updated environment file

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,5 @@
 export const environment = {
   firebaseConfig : {
-    apiKey: 'AIzaSyCuyMPg2_i8VAFr-C113r1uAckpADfNBwc',
-    authDomain: 'think-todo.firebaseapp.com',
-    projectId: 'think-todo',
-    storageBucket: 'think-todo.appspot.com',
-    messagingSenderId: '925563917151',
-    appId: '1:925563917151:web:951336641ce099fcf1dcea',
+    // Firebase Config
   },
 };
